1705975e240819d5a60e8c1aea57055fe1cafd7a
pratiksoni12 <pratiksoni2310@gmail.com>
2026-02-11 14:04:57 +0530

    BLEpage new file add ios

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,7 @@ import 'myprofile.dart';
 import 'mydoctor.dart';
 import 'myDevice.dart';
 import 'bleScan.dart';
+import 'blePage.dart';
 
 class HomeScreen extends StatefulWidget {
   static const routeName = '/home';
@@ -342,7 +343,8 @@ class _HomeScreenState extends State<HomeScreen> {
     Navigator.push(
       context,
       // MaterialPageRoute(builder: (_) => BleScan()),
-      MaterialPageRoute(builder: (_) => MyDevicesPage(userMobile: mobile)),
+      // MaterialPageRoute(builder: (_) => MyDevicesPage(userMobile: mobile)),
+      MaterialPageRoute(builder: (_) => BLEPage()),
 
 
     );
